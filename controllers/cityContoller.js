fde69b4d053de47a6171d0c1526a1c803035184e
Jerome Matthews <jerome_matthews@live.com>
2022-03-16 15:00:16 -0500

    +u added get single city functionality - find();

--- controllers/cityContoller.js
+++ controllers/cityContoller.js
@@ -79,27 +79,53 @@ what we use to identify the city that corresponds with the request.
 
 //  /:id - is the variable, it can be named anything.
 
-exports.getCity = (req, res) => {
-  console.log(req.params);
-  const cityName = req.params.id;
-  console.log(cityName);
+exports.getCity = async (req, res) => {
+  // Get a specific city using request parameters, manually from file.
+  // console.log(req.params);
+  // const cityName = req.params.id;
+  // console.log(cityName);
 
-  /* compares an object c - with all the objects in the citydata object array. 
-  If it finds an object with the property found in the variable: id, then it returns 
-  a new object with the founded city, storing it in thisCity.
-  */
-  const thisCity = cities.find((c) => c.Name === cityName);
+  // /* compares an object c - with all the objects in the citydata object array.
+  // If it finds an object with the property found in the variable: id, then it returns
+  // a new object with the founded city, storing it in thisCity.
+  // */
+  // const thisCity = cities.find((c) => c.Name === cityName);
 
-  if (!cityName) {
-    res.status(404).json({ message: 'No city found with this name:' });
-  } else {
+  // if (!cityName) {
+  //   res.status(404).json({ message: 'No city found with this name:' });
+  // } else {
+  //   res.status(200).json({
+  //     status: 'success',
+  //     data: {
+  //       thisCity,
+  //     },
+  //   });
+  // }
+
+  //Get a city from the mongo database using mongoose: findById();
+
+  try {
+    const cityName = req.params.id;
+    let cities = await City.find({ Name: cityName });
+    console.log(cityName);
     res.status(200).json({
-      status: 'success',
+      status: 'success, city found:',
       data: {
-        thisCity,
+        city: cities,
       },
     });
+  } catch (err) {
+    res.status(404).json({
+      status: 'error, city not found.',
+      message: err,
+    });
   }
+
+  //In the case of the City project API, we need to find the city name in the DB not really the ID of a given city, given that users will expect to search for a specific city by its name, not having any knowledge of the database, nor would anyone want to search, by some long, complex ID string. 
+  // Given this situation a query to the database usinf the find(); would be agumented to return the field name that matches the name taken from the request parameters object. I.E:  City.find({ Name: cityName });
+  //Taken what we learned from the Mongo Db couse with Max, and checking online for mongoose find() options. 
+
+  // In the example from the course, Jonas references the document IDs, and as such uses the findById() method.
 };
 
 exports.updateCity = (req, res) => {
