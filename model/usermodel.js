2ba836080313ea8359ef0f994c88b49b4ebed8ef
Jerome Matthews <jerome_matthews@live.com>
2022-04-11 15:32:14 -0500

    +u, installed & integrated JWT, pre method - model

--- model/usermodel.js
+++ model/usermodel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -16,10 +17,33 @@ const userSchema = new mongoose.Schema({
     type: String,
   },
   password: { type: String, required: true, minLength: 8 },
-  passwordConfirm: { 
-      type: String, 
-      required: [true, 'Please confirm your password'] },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      //remember that is only works on the save() function
+      validator: function (el) {
+        return el === this.password; // abc === xyz
+      },
+      message: 'Passwords are not the same.'
+    },
+  },
 });
 
+//To encrypt the password we make us of the Javascript implementation of the bcrypt library. Bcryptjs. We use the asynchronous function version as we're working with asynchronous codes in node. The function is on the mongoose middleware: pre action, of the Document middleware.
+
+//Middleware types: Document, Query, Aggregation, and Model - DQAM, Each can be attached in either pre, or post.
+userSchema.pre('save', async function(next) {
+
+  //Will only run is password was modified:
+  if(!this.isModified('password')) return next();
+//Hash password with cost of 12 
+this.password = await bcrypt.hash(this.password, 12) 
+//second parameter is the cost for SALTing the password, how cpu intensive the encryption algorithm should be. We're using the asynchronous version of the hash function. 
+
+//Delete passwordConfirm field.
+this.passwordConfirm = undefined;
+//NOTE: We can do this becuase the required field on passwordConfirm will enforce an entry into the passwordConfirm field, but it is not necessary to be persisted to the database. It's role in confirming the input of what is found in password is finished here. So can be emptied. 
+})
 const user = mongoose.model('User', userSchema);
 module.exports = user;
--- routes/cityroutes.js
+++ routes/cityroutes.js
@@ -2,13 +2,15 @@
 const express = require('express');
 const cityController = require('./../controllers/cityContoller');
 const router = express.Router();
-
+const authController = require('./../controllers/authController');
 //----------------------------------------------------------------
 /* express.json() allows the server to handle incoming JSON based requests. 
 Since the default is to expect text in the request body, this allows us to send the JSON
 data without specifying a request header. 
 */
 router.use(express.json());
+
+router.post('/signup', authController.signup);
 
 
 router
